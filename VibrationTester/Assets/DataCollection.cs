f26e22d457aff446357c75c56a1b1ab33658714e
NAmin23 <57247003+NAmin23@users.noreply.github.com>
2021-10-17 18:01:03 -0500

    Backend

---
+++ VibrationTester/Assets/DataCollection.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class DataCollection : MonoBehaviour
+{
+
+    //[SerializeField] InputField feedback1;
+
+    string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdg8k-bpGPQ3LPOd7M6pYCioKyzJVepYseN6pVydS7aVvn5yw/formResponse";
+
+
+    public void Send()
+    {
+        StartCoroutine(Post("hey there"));
+    }
+
+    IEnumerator Post(string s1)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.1637288442", s1);
+
+
+
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+
+    }
+
+
+}
